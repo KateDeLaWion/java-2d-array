3836153a4136c98d2e5a545b1cf44d9a9a80303d
Student101 <student101@PSAdmins-Air-3.home>
2020-04-18 21:23:28 -0400

    first commit

---
+++ src/array2d.java
@@ -0,0 +1,27 @@
+public class array2d {
+
+    public static void main(String[] args) {
+
+        //First row is   1 5
+        //Second row is  2 6
+        //Third row is   3 7
+        //Fourth row is  4 null
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+//         int myNumbers[][] = { {1, 2, 3, 4}, {5, 6, 7} };
+
+        // Can't just print 2D array
+//        System.out.println(myNumbers);
+//        System.out.println(myNumbers.length);
+//        System.out.println(myNumbers[0].length);
+//        System.out.println(myNumbers[1].length);
+
+//        for (int i = 0; i < myNumbers.length; ++i) {
+//            for(int j = 0; j < myNumbers[i].length; ++j) {
+//                System.out.println(myNumbers[i][j]);
+//            }
+//        }
+
+
+    }
+
+}
